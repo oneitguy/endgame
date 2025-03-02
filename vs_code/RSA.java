4309a43d5b7e21103a0076e18807b6b8a700fa12
oneitguy <73213795+oneitguy@users.noreply.github.com>
2025-03-02 22:54:11 +0530

    Update RSA.java

--- vs_code/RSA.java
+++ vs_code/RSA.java
@@ -1,43 +1,61 @@
-package vs_code;
+import java.util.*;
 import java.math.*;
-import java.security.*;
-
-public class RSA {
-    BigInteger p, q, n, d, e, ph, t;
-    SecureRandom r;
-
-    public RSA() {
-        r = new SecureRandom();
-        p = new BigInteger(2, 0, r);
-        q = new BigInteger(2, 0, r);
-        System.out.println("Prime numbers p and q are " + p.intValue() + ", " + q.intValue());
-        n = p.multiply(q);
-        ph = (p.subtract(new BigInteger("1")));
-        ph = ph.multiply(q.subtract(new BigInteger("1")));
-        e = new BigInteger("2");
-        while (ph.gcd(e).intValue() > 1 || e.compareTo(ph) != -1) {
-            e = e.add(new BigInteger("1"));
-            d = e.modInverse(ph);
-            System.out.println("Public key is (" + n.intValue() + "," + e.intValue() + ")");
-            System.out.println("Private key is (" + n.intValue() + "," + d.intValue() + ")");
-            BigInteger msg = new BigInteger("15");
-            System.out.println("\nMessage is: " + msg);
-            BigInteger enmsg = encrypt(msg, e, n);
-            System.out.println("\nEncrypted Message is: " + enmsg.intValue());
-            BigInteger demsg = decrypt(msg, d, n);
-            System.out.println("\nDecrypted Message is: " + demsg.intValue());
-        }
-    }
-
-    BigInteger encrypt(BigInteger msg, BigInteger e, BigInteger n) {
-        return msg.modPow(e, n);
-    }
-
-    BigInteger decrypt(BigInteger msg, BigInteger d, BigInteger n) {
-        return msg.modPow(d, n);
-    }
-
-    public static void main(String args[]) {
-        RSA r = new RSA();
-    }
+ 
+class RSA
+{
+ public static void main(String args[])
+ {
+ Scanner sc=new Scanner(System.in);
+ int p,q,n,z,d=0,e,i;
+ System.out.println("Enter the number to be encrypted and decrypted");
+ int msg=sc.nextInt();
+ double c;
+ BigInteger msgback; 
+ System.out.println("Enter 1st prime number p");
+ p=sc.nextInt();
+ System.out.println("Enter 2nd prime number q");
+ q=sc.nextInt();
+ 
+ n=p*q;
+ z=(p-1)*(q-1);
+ System.out.println("the value of z = "+z); 
+ 
+ for(e=2;e<z;e++)
+ {
+ if(gcd(e,z)==1) // e is for public key exponent
+ { 
+ break;
+ }
+ }
+ System.out.println("the value of e = "+e); 
+ for(i=0;i<=9;i++)
+ {
+ int x=1+(i*z);
+ if(x%e==0) //d is for private key exponent
+ {
+ d=x/e;
+ break;
+ }
+ }
+ System.out.println("the value of d = "+d); 
+ c=(Math.pow(msg,e))%n;
+ System.out.println("Encrypted message is : -");
+ System.out.println(c);
+                //converting int value of n to BigInteger
+ BigInteger N = BigInteger.valueOf(n);
+ //converting float value of c to BigInteger
+ BigInteger C = BigDecimal.valueOf(c).toBigInteger();
+ msgback = (C.pow(d)).mod(N);
+ System.out.println("Derypted message is : -");
+ System.out.println(msgback);
+ 
+ }
+ 
+ static int gcd(int e, int z)
+ {
+ if(e==0)
+ return z; 
+ else
+ return gcd(z%e,e);
+ }
 }

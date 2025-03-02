245354d780afe0b833f64d1512210c946486a9d9
oneitguy <73213795+oneitguy@users.noreply.github.com>
2025-03-02 22:53:35 +0530

    Update RailFence.java

--- vs_code/RailFence.java
+++ vs_code/RailFence.java
@@ -1,13 +1,22 @@
-package vs_code;
-class RailFence {
-    public static void main(String args[]) {
-        String input = "Good afternoon user.";
-        String output = "";
-        int len = input.length();
-        System.out.println("Input string: " + input);
-        for (int i = 0; i < len; i += 2) {
-            output += input.charAt(i);
+public class railfence {
+    public static void main(String[] args) {
+        String input = "Good afternoon my name is Parth.";
+        String cipherText = encryptRailFence(input);
+        System.out.println("Cipher text: " + cipherText);
+    }
+
+    public static String encryptRailFence(String text) {
+        StringBuilder row1 = new StringBuilder();
+        StringBuilder row2 = new StringBuilder();
+
+        // Arrange characters in rail fence pattern
+        for (int i = 0; i < text.length(); i++) {
+            if (i % 2 == 0)
+                row1.append(text.charAt(i));  // Even index - First row
+            else
+                row2.append(text.charAt(i));  // Odd index - Second row
         }
-        System.out.println("Cipher text: " + output);
+
+        return row1.toString() + row2.toString();
     }
 }

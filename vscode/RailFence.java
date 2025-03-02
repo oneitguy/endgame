19db2d6b642d03e3ce66f27877decc2970d81a19
onedarkguy <dark71429@gmail.com>
2025-03-02 06:33:55 +0530

    message

---
+++ vscode/RailFence.java
@@ -0,0 +1,12 @@
+class RailFence {
+    public static void main(String args[]) {
+        String input = "Good afternoon user.";
+        String output = "";
+        int len = input.length();
+        System.out.println("Input string: " + input);
+        for (int i = 0; i < len; i += 2) {
+            output += input.charAt(i);
+        }
+        System.out.println("Cipher text: " + output);
+    }
+}
